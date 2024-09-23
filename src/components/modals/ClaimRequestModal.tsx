8c70d46d3f0eccc72eefd3ad9c69d81c93f068c7
Sihab Hossain <sihabhossain380@gmail.com>
2024-09-23 17:19:56 +0600

    module done

--- src/components/modals/ClaimRequestModal.tsx
+++ src/components/modals/ClaimRequestModal.tsx
@@ -4,6 +4,7 @@ import FXInput from "../form/FXInput";
 import FXTextarea from "../form/FXTextArea";
 import FXModal from "./FXModal";
 import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useAddClaimRequest } from "@/src/hooks/claimRequest.hook";
 
 interface IProps {
   id: string;
@@ -11,7 +12,19 @@ interface IProps {
 }
 
 export default function ClaimRequestModal({ id, questions }: IProps) {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {};
+  const { mutate: handleClaimRequest, isPending } = useAddClaimRequest();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const claimRequestData = {
+      item: id,
+      description: data.description,
+      answers: Object.keys(data)
+        .filter((formElement) => formElement.startsWith("answer"))
+        .map((answer) => data[answer]),
+    };
+
+    handleClaimRequest(claimRequestData);
+  };
 
   return (
     <FXModal
@@ -34,7 +47,7 @@ export default function ClaimRequestModal({ id, questions }: IProps) {
 
         <div>
           <Button className="w-full flex-1 my-2" size="lg" type="submit">
-            Send
+            {isPending ? "Sending...." : "Send"}
           </Button>
         </div>
       </FXForm>
